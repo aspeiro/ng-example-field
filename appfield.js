87d5d19468b8168da0bc13d310c671612187c95f
Misael <misabitencourt@gmail.com>
2014-12-15 17:03:20 -0200

    first

---
+++ appfield.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name sampleApp.directive:appfield
+ * @description
+ * # appfield
+ */
+angular.module('sampleApp')
+  .directive('appfield', function () {
+    return {
+      template: '<label>{{ field.displayName }}</label><br><input type="{{ field.type }}" name="{{ field.name }}" ng-required="field.required" ng-model="inedition[field.name]" />',
+      restrict: 'E',
+      scope: {
+         field: '=',
+         inedition: '='
+      },
+      link: function postLink($scope) {
+      }
+    };
+  });
